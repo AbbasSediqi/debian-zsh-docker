309b9ba9b7c33fc285588036f751f2c1152285ac
AbbasSediqi <AbbasSediqi@gmail.com>
2022-05-06 21:20:49 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,9 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose;
+sudo systemctl start docker
 cat > /etc/docker/daemon.json <<EOF
 {
   "registry-mirrors": ["https://m.docker-registry.ir"]
